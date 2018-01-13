397e3a920f41b1e5ae00497c78c3b246210a03c0
mubashitachi <mohammed.mubashshir40@gmail.com>
2018-01-13 18:53:04 +0530

    home commit

--- index.js
+++ index.js
@@ -15,68 +15,68 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 //Configuring express app behaviour
 
-app.get("/MicroService", function (req, res) {
-    //res.send("Bot works");
-    console.log('Inside get method');
-    const header = {
-        'Cache-Control': 'no-cache',
-        Accept: 'application/json',
-        'Content-Type': 'application/json'
-    };
-    //https://limitless-beyond-94753.herokuapp.com/RailwayAPI
+// app.get("/MicroService", function (req, res) {
+//     //res.send("Bot works");
+//     console.log('Inside get method');
+//     const header = {
+//         'Cache-Control': 'no-cache',
+//         Accept: 'application/json',
+//         'Content-Type': 'application/json'
+//     };
+//     //https://limitless-beyond-94753.herokuapp.com/RailwayAPI
 
-    // let action = 'LaunchRequest';
-    // data.DataProcess[action];
+//     // let action = 'LaunchRequest';
+//     // data.DataProcess[action];
 
-    // async.parallel([
-    //     function (firstfn) {
-    //         console.log('Inside MicroService');
-    //         data.DataProcess[action];
-    //         firstfn(false, 'Data received Successfully');
-    //     }],
-    //     function (err, results) {
-    //         res.send("Bot works");
-    //         console.log(results);
-    //     });
+//     // async.parallel([
+//     //     function (firstfn) {
+//     //         console.log('Inside MicroService');
+//     //         data.DataProcess[action];
+//     //         firstfn(false, 'Data received Successfully');
+//     //     }],
+//     //     function (err, results) {
+//     //         res.send("Bot works");
+//     //         console.log(results);
+//     //     });
 
-    var options = {
-        url: 'https://limitless-beyond-94753.herokuapp.com/RailwayAPI', //,urlPath, //'https://api.railwayapi.com/v2/pnr-status/pnr/4338716830/apikey/sl5zmz3g1w'
-        method: 'POST',
-        header: header,
-        body: '',
-        json: true
-    };
+//     var options = {
+//         url: 'https://limitless-beyond-94753.herokuapp.com/RailwayAPI', //,urlPath, //'https://api.railwayapi.com/v2/pnr-status/pnr/4338716830/apikey/sl5zmz3g1w'
+//         method: 'POST',
+//         header: header,
+//         body: '',
+//         json: true
+//     };
 
-    request(options, function (error, response, body) {
-        if (error) {
-            console.dir(error);
-            return
-        }
-        console.log('headers:' + response.headers);
-        console.log('status code:' + response.statusCode);
-        console.log(body);
+//     request(options, function (error, response, body) {
+//         if (error) {
+//             console.dir(error);
+//             return
+//         }
+//         console.log('headers:' + response.headers);
+//         console.log('status code:' + response.statusCode);
+//         console.log(body);
 
-        console.log('Inside data process');
-    });
-});
+//         console.log('Inside data process');
+//     });
+// });
 //GET Endpoint
 
-app.post("/MicroService", function (req, res) {
-    let action = 'LaunchRequest';
+// app.post("/MicroService", function (req, res) {
+//     let action = 'LaunchRequest';
 
-    async.parallel([
-        function (firstfn) {
-            console.log('Inside MicroService');
-            data.DataProcess[action];
-            firstfn(false, 'Data received Successfully');
-        }],
-        function (err, results) {
-            console.log(results);
-        });
-    console.log(JSON.stringify(req.body.result.action));
+//     async.parallel([
+//         function (firstfn) {
+//             console.log('Inside MicroService');
+//             data.DataProcess[action];
+//             firstfn(false, 'Data received Successfully');
+//         }],
+//         function (err, results) {
+//             console.log(results);
+//         });
+//     console.log(JSON.stringify(req.body.result.action));
 
-    console.log('req.body.originalRequest.source');
-});
+//     console.log('req.body.originalRequest.source');
+// });
 
 app.post("/Bot", function (req, res) {
     console.log(JSON.stringify(req.body));
