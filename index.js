8ec7019a3d4fd6ea7781492a02f6ceb2352d1bec
Mubashshir Mohammed <39416@corp.hexaware.com>
2018-01-23 09:07:51 +0530

    work commit

--- index.js
+++ index.js
@@ -181,10 +181,10 @@ function CallAPI(request, response) {
                 msg = "An error occurred with API Microservice! Kindly contact adminsitrator";
                 commonFiles.sendMessage(response, msg);
             }
-            else if (!result[0][0].hasOwnProperty('response_code')) {
-                msg = "An error occurred with API Microservice! Kindly contact adminsitrator";
-                commonFiles.sendMessage(response, msg);
-            }
+            // else if (!result[0][0].hasOwnProperty('response_code')) {
+            //     msg = "An error occurred with API Microservice! Kindly contact adminsitrator";
+            //     commonFiles.sendMessage(response, msg);
+            // }
             else {
                 if (intentFrom === 'TrainIntent.CancelIntent') {
                     if (result[0][0].total > 0) {
